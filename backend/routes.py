086ebba6647aa5f212a81781de937556f666c157
BiciPeng <bicipeng@gmail.com>
2025-04-27 22:34:30 -0400

    added apis)

--- backend/routes.py
+++ backend/routes.py
@@ -11,9 +11,18 @@ def get_friends():
     result = [friend.to_json() for friend in friends]
     return jsonify(result)
 
-@app.route("/api/friends/id", methods=["GET"])
-def get_friend():
-    friend = Friend.query.one()
+# @app.route("/api/friends/<int:id>", methods=["GET"])
+# def get_friend():
+#     friend = Friend.query.get(id)
+#     if friend is None:
+#         return jsonify({"error":"friend does not exits!"}),404
+#     return jsonify(friend.to_json()),200
+# @app.route("/api/friends/<int:id>", methods=["GET"], strict_slashes=False)
+# def get_friend(id):
+#     friend = Friend.query.get(id)
+#     if friend is None:
+#         return jsonify({"error": "Friend not found"}), 404
+#     return jsonify(friend.to_json()), 200
 
 #create a friend 
 @app.route("/api/friends", methods=["POST"])
@@ -21,6 +30,12 @@ def create_friend():
     try:
         # get data from the request body
         data = request.json
+
+        requried_fields = ["name","gender","role","description"]
+        for field in requried_fields:
+            if field not in data:
+             return jsonify({f"msg":"{field} field is required"}), 201  
+
         name = data.get("name")
         gender = data.get("gender")
         role = data.get("role")
@@ -39,6 +54,45 @@ def create_friend():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error":str(e)}), 500
+    
+#delete
+@app.route("/api/friends/<int:id>", methods = ["DELETE","PATCH"])
+def handle_friend(id):
+    friend = Friend.query.get(id)
+    if friend is None:
+        return jsonify({"error":"Friend not found !"}),404
+    if request.method == "DELETE":
+        try:
+            db.session.delete(friend)
+            db.session.commit()
+            return jsonify({"msg":"Friend deleted successfully"}), 200
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error":str(e)}), 500 
+    elif request.method == "PATCH":
+        try:
+        # get the specific from the request body
+            data = request.get_json(force=True)
+            # get the name from request body, if no name has been past, use the original name of the friend
+            friend.name = data.get("name", friend.name)
+            friend.gender = data.get("gender",friend.gender)
+            friend.role = data.get("role",friend.role)
+            friend.description = data.get("description",friend.description)
+            db.session.commit()
+            return jsonify({"msg":"Friend Updated successfully"}), 200
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error":str(e)}), 500
+
+
+
+# def delete_freind(id):
+
+    
+# #update a friend 
+# @app.route("/api/friends/<int:id>", methods=["PATCH"])
+# def update_friend(id):
+
     
 
     
